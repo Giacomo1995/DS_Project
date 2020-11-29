6ea5126392f8600abe4d2379206719c124ae759a
Giacomo1995 <giamel95@outlook.it>
2020-11-29 14:25:19 +0100

    Implemented Farthest Insertion algorithm

---
+++ farthest_insertion.py
@@ -0,0 +1,75 @@
+import math
+import sys
+import random
+import time
+import numpy as np
+from copy import copy, deepcopy
+
+
+def farthest_insertion(G, starting_node = 0):
+    inf = sys.float_info.max
+    N = len(G[0])
+    M = deepcopy(G)
+    C = []  # Cycle C (initialized to empty set)
+
+    for i in range(N):
+        M[i][i] = -inf
+        G[i][i] = inf
+
+    t0 = time.time()  # Starting the timer
+
+    C.append(starting_node)
+    C.append(M[starting_node].index(max(M[starting_node])))
+
+    for i in range(N):
+        #i = random.choice(range(len(C)))
+        max_found = False
+
+        # Find the the node with maximum cost to any node of C and add it to the cycle
+        while(max_found == False):
+            j = M[i].index(max(M[i]))  # Finds the node j such that arc (i, j) with maximum cost (for all arcs steaming from i)
+
+            if ((j in C) == False):
+                new_node = j
+                max_found = True
+            else:
+                M[i][j] = -inf
+
+        # Find the arc to delete in order to insert the new node in cycle C
+        min_cost = inf
+        for k in range(len(C)):
+            h = (k+1) % len(C)
+
+            tmp = G[C[k]][new_node] + G[new_node][C[h]] - G[C[k]][C[h]]
+            if (tmp < min_cost):
+                min_cost = tmp
+                arc = [k, h]
+
+        if (arc[1] == 0):
+            arc[1] = len(C)
+
+        C = C[0:(arc[0]+1)] + [new_node] + C[arc[1]:]  # Updates cycle C
+
+        if (len(C) == len(G[i])):
+            print("Time: " + str(time.time() - t0))
+            return C
+
+
+# TEST
+
+# Cost matrix
+inf = sys.float_info.max
+G = [[inf, 6.12, 11.1, 4.33, 3.83],
+    [2.12, inf, 8.56, 5.72, 2.61],
+    [6.1, 7.56, inf, 4.25, 4.33],
+    [7.33, 12.72, 12.25, inf, 8.33],
+    [1.83, 4.61, 7.33, 3.33, inf]]
+output = farthest_insertion(G)
+print(output)
+
+final_cost = 0
+for i in range(len(output)):
+    final_cost = final_cost + G[output[i]][(output[(i+1)%len(output)])]
+    print("(" + str(output[i]) + ", " + str(output[(i+1)%len(output)]) + "): " + str(G[output[i]][(output[(i+1)%len(output)])]))
+
+print("Final cost: " + str(final_cost))
